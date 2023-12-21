2a8a53667c34a2833936c960043336f070545df6
Ammario711 <64035831+Ammario711@users.noreply.github.com>
2023-12-21 04:49:54 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+
+"""Welcome to the Calculator program!")
+Select an operation to perform by inputting a number:
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division"""
+
+operation = input("""Welcome to the Calculator program! 
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
+Select an operation to perform by inputting a number: """)
+
+if operation == "1":
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+    print(int(num1)+int(num2))
+elif operation == "2":
+    #subtraction
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+    print(int(num1) - int(num2))
+elif operation == "3":
+    #multiplication
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+    print(int(num1) * int(num2))
+elif operation == "4":
+    #division
+    num1 = input("Enter first number: ")
+    num2 = input("Enter second number: ")
+    print(int(num1) / int(num2))
+else:
+    print("Error: Please input a number that corresponds with the operations on the list")
+
+
+
+
+
+
+
